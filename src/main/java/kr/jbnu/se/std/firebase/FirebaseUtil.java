bbea7d95aa14f82e73af7e900dd650e35bae95e3
jaewoo-rain <202017499@jbnu.ac.kr>
2024-10-15 19:41:14 +0900

    login UI 완성

---
+++ src/main/java/kr/jbnu/se/std/firebase/FirebaseUtil.java
@@ -0,0 +1,36 @@
+package kr.jbnu.se.std.firebase;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class FirebaseUtil {
+    private static Firestore db;
+
+    public static Firestore getFirestore() {
+        if (db == null) {
+            initializeFirebase();
+        }
+        return db;
+    }
+
+    private static void initializeFirebase() {
+        try {
+            FileInputStream serviceAccount = new FileInputStream("src/main/resources/serviceAccountKey.json");
+
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setDatabaseUrl("https://shoottheduck-29c34.firebaseio.com")
+                    .build();
+            FirebaseApp.initializeApp(options);
+            db = FirestoreClient.getFirestore();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
